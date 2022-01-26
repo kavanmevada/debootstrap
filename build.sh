e0d6ff0fceb45f9aa64092b3ba3cf2af96deb04f
Kavan Mevada <kavanmevada@gmail.com>
2022-01-26 17:01:20 +0530

    ...

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
-podman build --no-cache -t ghcr.io/kavanmevada/debootstrap-iso:host-system . --file host-system
-podman build --no-cache -t ghcr.io/kavanmevada/debootstrap-iso:host-environment . --file host-environment
-podman build --no-cache -t ghcr.io/kavanmevada/debootstrap-iso:tools-binutils . --file tools-binutils
-podman build --no-cache -t ghcr.io/kavanmevada/debootstrap-iso:tools-gcc . --file tools-gcc
-podman build --no-cache -t ghcr.io/kavanmevada/debootstrap-iso:tools-gcc . --file tools-linux-api-headers
+echo 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 | podman login ghcr.io -u kavanmevada --password-stdin
+podman build --squash -t ghcr.io/kavanmevada/debootstrap-iso:host-system . --file host-system
+podman build --squash -t ghcr.io/kavanmevada/debootstrap-iso:host-environment . --file host-environment
+podman build --squash -t ghcr.io/kavanmevada/debootstrap-iso:tools-binutils . --file tools-binutils
+podman build --squash -t ghcr.io/kavanmevada/debootstrap-iso:tools-gcc . --file tools-gcc
+podman build --squash -t ghcr.io/kavanmevada/debootstrap-iso:tools-linux-api-headers . --file tools-linux-api-headers
+podman build --squash -t ghcr.io/kavanmevada/debootstrap-iso:tools-glibc . --file tools-glibc
+podman build --squash -t ghcr.io/kavanmevada/debootstrap-iso:tools-libstdcpp . --file tools-libstdcpp
+podman build --squash -t ghcr.io/kavanmevada/debootstrap-iso:cross-m4 . --file cross-m4
+podman build --squash -t ghcr.io/kavanmevada/debootstrap-iso:cross-ncurses . --file cross-ncurses
